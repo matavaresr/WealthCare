925a619607ecf8b6fca593088c4eb8eaac3e99c3
Marco Tavares <matr192001@hotmail.com>
2024-05-25 11:57:33 -0600

    Seconda change

--- index.js
+++ index.js
@@ -10,6 +10,28 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/login', (req, res) => {
+    res.send('Esto es un dashboard!');
+});
+
+app.get('/signup', (req, res) => {
+  res.send('Esto es un dashboard!');
+});
+
+app.get('/planesdeahorro', (req, res) => {
+  res.send('Esto es un dashboard!');
+});
+
+
+app.get('/data', (req, res) => {
+  res.send('Esto es un dashboard!');
+});
+
+
+app.use((req, res)=> {
+  res.send('No se encontro ni una pagina crack');
+})
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
