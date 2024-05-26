6afda51328f38b134cd10a263edcb97c241c1fdb
Marco Tavares <matr192001@hotmail.com>
2024-05-25 23:45:39 -0600

    Commit sobre Pug con Views

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     const appDiv = document.getElementById('app');
     appDiv.innerHTML = '<p>¡Hola, esta es una aplicación de Express empaquetada con webpack!</p>';
-  });
+});
   
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Configura el motor de plantillas Pug
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware para servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'public'))); // Servir archivos estáticos desde 'public'
+app.use(express.static(path.join(__dirname, 'dist'))); // Servir archivos estáticos desde 'dist'
+
+// Define una ruta para renderizar una vista Pug
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Mi aplicación', message: '¡Hola, mundo!' });
+});
+
+app.get('/home', (req, res) => {
+    res.render('inicio', { title: 'Mi aplicación', message: '¡Hola, mundo!' });
+});
+
+app.get('/signup', (req, res) => {
+    res.render('crearCuenta', { title: 'Mi aplicación', message: '¡Hola, mundo!' });
+});
+
+app.get('/planes', (req, res) => {
+    res.render('planes', { title: 'Mi aplicación', message: '¡Hola, mundo!' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
