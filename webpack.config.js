b13ebea1d0312252a1e951c2884fbd75fa65ea6b
Marco Tavares <matr192001@hotmail.com>
2024-05-26 02:32:44 -0600

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './index.js', // Ruta a tu archivo principal de JS
+  entry: './server.js', // Ruta a tu archivo principal de JS
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'), // Directorio de salida
